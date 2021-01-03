8dfec28b6a6b8ff998b2f39c146584ca312ee671
gabbs <gortman@rednovalabs.com>
2021-01-03 14:33:23 -0600

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<span>new app</span>, document.getElementById('root'));
